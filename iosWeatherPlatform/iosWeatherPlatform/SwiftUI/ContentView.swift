54a4a65746fdf74fecd4471ce1eb749a1528a5ca
Russell Stewart <russell@zmosoft.com>
2024-02-13 21:51:23 -0700

    iOS: Removing some log statements.

--- iosWeatherPlatform/iosWeatherPlatform/SwiftUI/ContentView.swift
+++ iosWeatherPlatform/iosWeatherPlatform/SwiftUI/ContentView.swift
@@ -7,7 +7,6 @@ struct ContentView: View {
     
     var body: some View {
         VStack {
-            let _ = print("MainScreenState: mainScreenState.state = \(mainScreenState.state)")
             WeatherSearchBar(
                 loading: (mainScreenState.state is MainScreenStateWeatherDataLoading),
                 onLocationSearch: { query in
@@ -22,14 +21,12 @@ struct ContentView: View {
             )
             
             if let autocompletePredictions = (mainScreenState.state as? MainScreenStateAutocompleteLoaded)?.places {
-                let _ = print("MainScreenState: Autocomplete")
                 LocationAutocompleteResultsView(
                     autocompletePredictions: autocompletePredictions
                 ) { location in
                     mainScreenState.onLocationSelected(location: location)
                 }
             } else if let loading = (mainScreenState.state as? MainScreenStateWeatherDataLoading) {
-                let _ = print("MainScreenState: Loading")
                 ZStack {
                     WeatherDataView(data: loading.data)
                     
@@ -37,10 +34,8 @@ struct ContentView: View {
                         .padding(6)
                 }
             } else if let data = (mainScreenState.state as? MainScreenStateWeatherData)?.data {
-                let _ = print("MainScreenState: Loaded")
                 WeatherDataView(data: data)
             } else if let error = (mainScreenState.state as? MainScreenStateError)?.error, !error.isEmpty {
-                let _ = print("MainScreenState: Error")
                 if (!error.isEmpty) {
                     ErrorView(error: error)
                 }
